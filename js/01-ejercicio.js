cba6efef6ee97696459fbd0f5509b57907700198
Jochyd <daoua.josefina@gmail.com>
2022-07-10 18:30:57 -0300

    primer commit

---
+++ js/01-ejercicio.js
@@ -0,0 +1,8 @@
+let edad = prompt('Cual es tu edad? (si sos mayor de 99 años no puedes participar)')
+
+edad = 18;
+do{
+    for(edad >=18; edad < 50; edad++){
+        console.log('<br>ya puedes conducir</br>');
+   }
+}while(edad)
