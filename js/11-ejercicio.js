c1f88a943a11087b8e51ce70aa397cfc4d5dbb87
Jochyd <daoua.josefina@gmail.com>
2022-07-10 20:36:03 -0300

    termine el ejercicio 11

--- js/11-ejercicio.js
+++ js/11-ejercicio.js
@@ -10,14 +10,41 @@ let edadPerson3 = parseInt(prompt("Ingrese su edad"));
 if (edadPerson1 === edadPerson2 && edadPerson2 === edadPerson3) {
   document.write("todas las personas tienen la misma edad");
 } else {
-  if (edadPerson1 == Math.max(edadPerson1, edadPerson2, edadPerson3)) {
-    document.write(person1 + "es el mayor");
+  if (
+    edadPerson1 === edadPerson2 &&
+    edadPerson1 === Math.max(edadPerson1, edadPerson2, edadPerson3)
+  ) {
+    document.write(
+      person1 + " y " + person2 + " son los mayores y tienen " + edadPerson1
+    );
   } else {
-    if (edadPerson2 == Math.max(edadPerson1, edadPerson2, edadPerson3)) {
-      document.write(person2 + "es el mayor");
+    if (
+      edadPerson2 === edadPerson3 &&
+      edadPerson2 === Math.max(edadPerson1, edadPerson2, edadPerson3)
+    ) {
+      document.write(
+        person2 + " y " + person3 + " son los mayores y tienen " + edadPerson2
+      );
     } else {
-      if (edadPerson3 == Math.max(edadPerson1, edadPerson2, edadPerson3))
-        document.write(person3 + "es el mayor");
+      if (
+        edadPerson1 === edadPerson3 &&
+        edadPerson1 === Math.max(edadPerson1, edadPerson2, edadPerson3)
+      ) {
+        document.write(
+          person1 + " y " + person3 + " son los mayores y tienen " + edadPerson1
+        );
+      } else {
+        if (edadPerson1 === Math.max(edadPerson1, edadPerson2, edadPerson3)) {
+          document.write(person1 + "es el mayor");
+        } else {
+          if (edadPerson2 === Math.max(edadPerson1, edadPerson2, edadPerson3)) {
+            document.write(person2 + "es el mayor");
+          } else {
+            if (edadPerson3 === Math.max(edadPerson1, edadPerson2, edadPerson3))
+              document.write(person3 + "es el mayor");
+          }
+        }
+      }
     }
   }
 }
